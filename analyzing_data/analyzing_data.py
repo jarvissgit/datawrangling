8ded7f41a410fbe5c0037fdb4b3b4fd21ace77f4
J P <jitkp85@gmail.com>
2017-03-29 17:28:16 +0530

    added screenshot explaining aggregation pipeline

--- analyzing_data/analyzing_data.py
+++ analyzing_data/analyzing_data.py
@@ -12,6 +12,17 @@ db = client.twitter
 
 def most_tweets():
     result = db.tweets.aggregate([
-        { "$group" : 
+        { "$group" : { "_id" : "$user.screen_name",
+                      "count" : { "$sum" : 1 } } },
+        { "$sort" : { "count" : -1 } } ])
+    return result
+# $user.screen_name means to group all user.screen_name entries based on the value of user.screen_name
+# $sum is the accumulator operator
+# the first line after $group says that for each same value of user.screen_name, increment the value of count by 1
 
+#refer aggregation_pipeline.png
+
+if __name__ == '__main__':
+    result = most_tweets()
+    pprint.pprint(result)
 
