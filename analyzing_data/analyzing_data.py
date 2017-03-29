04be2425e23367116c6ea6db6cce9dc884bf4786
J P <jitkp85@gmail.com>
2017-03-29 16:24:39 +0530

    last saved file for mongodb analyzing data

--- analyzing_data/analyzing_data.py
+++ analyzing_data/analyzing_data.py
@@ -1,1 +1,17 @@
-#First line
+#WHO TWEETED MOST?
+#-group tweets by user
+#-count each user's tweet
+#-sort into descending order
+#-select user at top
+
+from pymongo import MongoClient
+import pprint
+
+client = MongoClient("mongodb://localhost:27017")
+db = client.twitter
+
+def most_tweets():
+    result = db.tweets.aggregate([
+        { "$group" : 
+
+
