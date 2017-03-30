993060883f7f900d45c3d29b530b507d33501f38
J P <jitkp85@gmail.com>
2017-03-30 23:19:30 +0530

    push & addToSet

--- analyzing_data/analyzing_data.py
+++ analyzing_data/analyzing_data.py
@@ -351,4 +351,139 @@ if __name__ == '__main__':
     assert result[0]["_id"] == "Uttar Pradesh"
     assert result[0]["count"] == 623
 
-# This code works well
+"""
+
+NOTE: In the current version of pymongo (3.0), aggregation operations return a cursor object. In order to see the elements returned, you can iterate over the cursor object, such as using a for loop, and print out the elements one by one. Keep this in mind when working on your local machine.
+
+This is an example document:
+
+{
+    "_id" : ObjectId("52fe1d364b5ab856eea75ebc"),
+    "elevation" : 1855,
+    "name" : "Kud",
+    "country" : "India",
+    "lon" : 75.28,
+    "lat" : 33.08,
+    "isPartOf" : [
+        "Jammu and Kashmir",
+        "Udhampur district"
+    ],
+    "timeZone" : [
+        "Indian Standard Time"
+    ],
+    "population" : 1140
+}
+Tip: Don't forget that we are concerned only with regions in India when you are constructing your pipeline.
+
+"""
+# THIS CODE WORKS WELL!!!
+
+
+#---------------------------------
+# GROUP ACCUMULATION OPERATORS
+#---------------------------------
+
+# $sum
+
+# $first
+
+# $last
+
+# $max
+
+# $min
+
+# $avg
+def hashtag_retweet_avg():
+    result = db.tweets.aggregate([
+        { "$unwind" : { "id" : "$entities.hashtags.text",
+                       "retweet_avg" : {"$avg" : "$retweet_count"}
+                      } },
+        { "$sort" : { "retweet_avg" : -1 } } ] )
+    
+    return result
+
+if __name__ == '__main__':
+    result = hashtag_retweet_avg()
+    pprint.pprint(result)
+
+# $push [deals with arrays]
+
+
+# $addToSet [deals with arrays, adds to the array like a set i.e. does not add duplicate values, ]
+def unique_hashtags_by_user():
+    result = db.tweets.aggregate([
+        { "$unwind" : "$entities.hashtags"},
+        { "$group" : { "_id" : "$user.screen_name",
+                      "unique_hashtags" : {
+                          "$addToSet" : "$entities.hashtags.text"
+                      } } },
+        { "$sort" : { "_id" : -1 } } ] )
+    
+    return result
+
+if __name__ == '__main__':
+    result = unique_hashtags_by_user()
+
+#-----------------------------------------------------------
+#QUIZ : using push
+#----------------------------------------------------------
+
+#!/usr/bin/env python
+"""
+$push is similar to $addToSet. The difference is that rather than accumulating only unique values 
+it aggregates all values into an array.
+
+Using an aggregation query, count the number of tweets for each user. In the same $group stage, 
+use $push to accumulate all the tweet texts for each user. Limit your output to the 5 users
+with the most tweets. 
+Your result documents should include only the fields:
+"_id" (screen name of user), 
+"count" (number of tweets found for the user),
+"tweet_texts" (a list of the tweet texts found for the user).  
+
+Please modify only the 'make_pipeline' function so that it creates and returns an aggregation 
+pipeline that can be passed to the MongoDB aggregate function. As in our examples in this lesson, 
+the aggregation pipeline should be a list of one or more dictionary objects. 
+Please review the lesson examples if you are unsure of the syntax.
+
+Your code will be run against a MongoDB instance that we have provided. If you want to run this code 
+locally on your machine, you have to install MongoDB, download and insert the dataset.
+For instructions related to MongoDB setup and datasets please see Course Materials.
+
+Please note that the dataset you are using here is a smaller version of the twitter dataset used in 
+examples in this lesson. If you attempt some of the same queries that we looked at in the lesson 
+examples, your results will be different.
+"""
+
+def get_db(db_name):
+    from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    db = client[db_name]
+    return db
+
+def make_pipeline():
+    # complete the aggregation pipeline
+    pipeline = [{"$group" : { "_id" : "$user.screen_name",
+                            "count" : {"$sum" : 1},
+                            "tweet_texts" : {"$push": "$text"}}},
+                {"$sort" : {"$count": -1}},
+                {"$limit":5}]
+    return pipeline
+
+def aggregate(db, pipeline):
+    return [doc for doc in db.twitter.aggregate(pipeline)]
+
+
+if __name__ == '__main__':
+    db = get_db('twitter')
+    pipeline = make_pipeline()
+    result = aggregate(db, pipeline)
+    import pprint
+    pprint.pprint(result)
+    assert len(result) == 5
+    assert result[0]["count"] > result[4]["count"]
+    sample_tweet_text = u'Take my money! #liesguystell http://movie.sras2.ayorganes.com'
+    assert result[4]["tweet_texts"][0] == sample_tweet_text
+   
+#some error in the pipeline 
