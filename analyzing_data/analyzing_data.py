dab8846754aaa1c1b567cc99bebc2f2c0c420274
J P <jitkp85@gmail.com>
2017-04-01 13:17:16 +0530

    multiple stages same operator

--- analyzing_data/analyzing_data.py
+++ analyzing_data/analyzing_data.py
@@ -487,3 +487,164 @@ if __name__ == '__main__':
     assert result[4]["tweet_texts"][0] == sample_tweet_text
    
 #some error in the pipeline 
+
+"""
+NOTE: In the current version of pymongo (3.0), aggregation operations return a cursor object. In order to see the elements returned, you can iterate over the cursor object, such as using a for loop, and print out the elements one by one. Keep this in mind when working on your local machine.
+
+Example Tweet:
+
+{
+    "_id" : ObjectId("5304e2e3cc9e684aa98bef97"),
+    "text" : "First week of school is over :P",
+    "in_reply_to_status_id" : null,
+    "retweet_count" : null,
+    "contributors" : null,
+    "created_at" : "Thu Sep 02 18:11:25 +0000 2010",
+    "geo" : null,
+    "source" : "web",
+    "coordinates" : null,
+    "in_reply_to_screen_name" : null,
+    "truncated" : false,
+    "entities" : {
+        "user_mentions" : [ ],
+        "urls" : [ ],
+        "hashtags" : [ ]
+    },
+    "retweeted" : false,
+    "place" : null,
+    "user" : {
+        "friends_count" : 145,
+        "profile_sidebar_fill_color" : "E5507E",
+        "location" : "Ireland :)",
+        "verified" : false,
+        "follow_request_sent" : null,
+        "favourites_count" : 1,
+        "profile_sidebar_border_color" : "CC3366",
+        "profile_image_url" : "http://a1.twimg.com/profile_images/1107778717/phpkHoxzmAM_normal.jpg",
+        "geo_enabled" : false,
+        "created_at" : "Sun May 03 19:51:04 +0000 2009",
+        "description" : "",
+        "time_zone" : null,
+        "url" : null,
+        "screen_name" : "Catherinemull",
+        "notifications" : null,
+        "profile_background_color" : "FF6699",
+        "listed_count" : 77,
+        "lang" : "en",
+        "profile_background_image_url" : "http://a3.twimg.com/profile_background_images/138228501/149174881-8cd806890274b828ed56598091c84e71_4c6fd4d8-full.jpg",
+        "statuses_count" : 2475,
+        "following" : null,
+        "profile_text_color" : "362720",
+        "protected" : false,
+        "show_all_inline_media" : false,
+        "profile_background_tile" : true,
+        "name" : "Catherine Mullane",
+        "contributors_enabled" : false,
+        "profile_link_color" : "B40B43",
+        "followers_count" : 169,
+        "id" : 37486277,
+        "profile_use_background_image" : true,
+        "utc_offset" : null
+    },
+    "favorited" : false,
+    "in_reply_to_user_id" : null,
+    "id" : NumberLong("22819398300")
+}
+
+"""
+#-----------------------------------------------------------
+# MULTIPLE STAGES USING A GIVEN OPERATOR
+#----------------------------------------------------------
+
+#who has mentioned the most unique users?
+
+#accoriding to my understanding, if a unique user is mentioned, then the entities.user_mentions.screen_name field will exist. Otherwise, it will not exist
+
+def unique_user_mentions():
+    result = db.tweets.aggregate([
+        { "$unwind" : "entities.user_mentions" },
+        { "$group" : {
+            "_id" : "$user.screen_name",
+            "mset" : {
+                "$addToSet" : "$entities.user_mentions.screen_name"
+            } } },
+        { "$unwind" : "$mset"},
+        { "$group" : { "_id" : "$_id",
+                      "count" : { "$sum" : 1 } } },
+        { "$sort" : { "count" : -1} },
+        { "$limit" : 10 } ] )
+
+#-----------------------------------------------------------
+# QUIZ: SAME OPERATOR
+#----------------------------------------------------------
+#!/usr/bin/env python
+"""
+In an earlier exercise we looked at the cities dataset and asked which region in India contains 
+the most cities. In this exercise, we'd like you to answer a related question regarding regions in 
+India. What is the average city population for a region in India? Calculate your answer by first 
+finding the average population of cities in each region and then by calculating the average of the 
+regional averages.
+
+Hint: If you want to accumulate using values from all input documents to a group stage, you may use 
+a constant as the value of the "_id" field. For example, 
+    { "$group" : {"_id" : "India Regional City Population Average",
+      ... }
+
+Please modify only the 'make_pipeline' function so that it creates and returns an aggregation 
+pipeline that can be passed to the MongoDB aggregate function. As in our examples in this lesson, 
+the aggregation pipeline should be a list of one or more dictionary objects. 
+Please review the lesson examples if you are unsure of the syntax.
+
+Your code will be run against a MongoDB instance that we have provided. If you want to run this code 
+locally on your machine, you have to install MongoDB, download and insert the dataset.
+For instructions related to MongoDB setup and datasets please see Course Materials.
+
+Please note that the dataset you are using here is a smaller version of the twitter dataset used 
+in examples in this lesson. If you attempt some of the same queries that we looked at in the lesson 
+examples, your results will be different.
+"""
+
+def get_db(db_name):
+    from pymongo import MongoClient
+    client = MongoClient('localhost:27017')
+    db = client[db_name]
+    return db
+
+def make_pipeline():
+    # complete the aggregation pipeline
+    pipeline = [ ]
+    return pipeline
+
+def aggregate(db, pipeline):
+    return [doc for doc in db.cities.aggregate(pipeline)]
+
+
+if __name__ == '__main__':
+    db = get_db('examples')
+    pipeline = make_pipeline()
+    result = aggregate(db, pipeline)
+    assert len(result) == 1
+    # Your result should be close to the value after the minus sign.
+    assert abs(result[0]["avg"] - 201128.0241546919) < 10 ** -8
+    import pprint
+    pprint.pprint(result)
+#didn't try this quiz
+
+"""
+{
+    "_id" : ObjectId("52fe1d364b5ab856eea75ebc"),
+    "elevation" : 1855,
+    "name" : "Kud",
+    "country" : "India",
+    "lon" : 75.28,
+    "lat" : 33.08,
+    "isPartOf" : [
+        "Jammu and Kashmir",
+        "Udhampur district"
+    ],
+    "timeZone" : [
+        "Indian Standard Time"
+    ],
+    "population" : 1140
+}
+"""
