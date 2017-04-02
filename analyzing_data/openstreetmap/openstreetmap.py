975fb10de7619c279e598a4df037c2bd859ffc1c
J P <km4gck@gmail.com>
2017-04-02 12:41:36 +0530

    changed directory

---
+++ analyzing_data/openstreetmap/openstreetmap.py
@@ -0,0 +1,15 @@
+# ---------------------------------------------
+# CASE STUDY : OPENSTREET MAP
+# ---------------------------------------------
+# - available for download
+# - XML
+# - human edited
+# - relatable
+# - open source
+
+# --------------------
+# Familiarize yourself
+# -------------------
+# nodes, ways and relations from the OSM XML documentation
+
+
